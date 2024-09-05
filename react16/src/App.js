131238b384dae2c76f977b93f5d1017e0b6a74d8
jsoyun <syusmm233@gmail.com>
2024-09-05 22:22:37 +0900

    feature/react16

---
+++ react16/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Head from "./Head";
+
+function App() {
+  return (
+    <div>
+      <Head />
+      <h1>Hello, world!</h1>
+    </div>
+  );
+}
+export default App;
---
+++ react16/src/Head.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Head() {
+  return <header>이것은 헤드다</header>;
+}
+
+export default Head;
